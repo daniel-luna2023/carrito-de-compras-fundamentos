4fa652b9f0402456063a547282d678741668279b
oscar_gomez <oscargomezluna@gmail.com>
2023-07-13 11:20:40 -0500

    porfin

---
+++ assets/js/components/loader.js
@@ -0,0 +1,8 @@
+function loader () {
+   window.addEventListener('load',function() {
+    const loader = document.querySelector('.loader')
+    loader.classList.add('loader__hiden')
+   }) 
+}
+
+export default loader
---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+import loader from "./components/loader.js";
+import showmenu from "./components/showMenu.js";
+import showcart from "./components/showCart.js";
+
+/* * UI Element */
+/* ocultar loader */
+loader()
+
+/* Mostrar menu */
+showmenu()
+
+/* Mostrar Cart */
+showcart()
+
+
+/* End UI Elements */
+
+
+/* Product*/
+
